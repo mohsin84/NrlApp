502b7f4ebbeb3b002f80566f6a0dc75e3c787ae3
Mohsin Reza <mohsin.reza@optus.com.au>
2018-07-22 13:28:26 +1000

    Default player image

--- app/src/main/java/mohsin/reza/nrlapp/binding/FragmentBindingAdapters.kt
+++ app/src/main/java/mohsin/reza/nrlapp/binding/FragmentBindingAdapters.kt
@@ -4,14 +4,20 @@ import android.databinding.BindingAdapter
 import android.support.v4.app.Fragment
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import mohsin.reza.nrlapp.R
 import javax.inject.Inject
 
 class FragmentBindingAdapters @Inject constructor(internal val fragment: Fragment) {
     @BindingAdapter("circleImageUrl")
     fun bindCircleImage(imageView: ImageView, url: String) { //to bind dynamic url in recyclerView
+
         val context = fragment.activity!!.applicationContext
+        val requestOption = RequestOptions().centerCrop().error(R.mipmap.headshot_error)
+
         Glide.with(context)
                 .load(url)
+                .apply(requestOption)
                 .into(imageView)
         //TODO: add default url for error
     }
